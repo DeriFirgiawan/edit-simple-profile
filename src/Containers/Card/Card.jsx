4ac27b33dedbb26e93df467d83d30facab31093f
DeriFirgiawan <derifirgiawan025@gmail.com>
2020-09-15 15:57:35 +0700

    first commit

---
+++ src/Containers/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import FormEditCard from '../FormEditCard/FormEditCard';
+class Card extends Component {
+  render() {
+    return <FormEditCard />;
+  }
+}
+
+export default Card;
---
+++ src/Containers/FormEditCard/FormEditCard.jsx
@@ -0,0 +1,63 @@
+import React, { Component, Fragment } from 'react';
+import './FormEditCard.css';
+import CardProfile from '../../Components/CardProfile/CardProfile';
+
+class FormEditCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'Deri Firgiawan',
+      opened: false,
+      username: '@infineteloop_25'
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleBox = this.toggleBox.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    console.log(this.state.value);
+  }
+
+
+  handleSubmit(event) {
+    alert(`Update nama Berhasil menjadi ${this.state.value}`);
+    event.preventDefault();
+  }
+
+  toggleBox() {
+    const {opened} = this.state;
+    this.setState({
+      opened: !opened
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <section className="section-profile">
+          <CardProfile name={this.state.value} userName={this.state.username} title="Edit" click={this.toggleBox}/>
+          {
+            this.state.opened? <div className="card-edit-name">
+                                <form onSubmit={this.handleSubmit}>
+                                  <label>
+                                    Name :
+                                    <input
+                                      type="text"
+                                      value={this.state.value}
+                                      onChange={this.handleChange}
+                                    />
+                                  </label>
+                <input type="submit" value="Save" onClick={this.toggleBox}/>
+                                </form>
+                              </div>: null
+          }
+        </section>
+        
+      </Fragment>
+    );
+  }
+}
+
+export default FormEditCard;
